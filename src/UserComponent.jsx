f64ed0783c9fa5a82df264f10cefb638656688c7
venkatanikhil58 <venkatanikhil@lyrostech.com>
2024-06-19 17:27:48 +0530

    02task

---
+++ src/UserComponent.jsx
@@ -0,0 +1,15 @@
+function Person() {
+    return(
+        <>
+        <h1>This is person component</h1>
+        </>
+    )
+}
+function UserComponent() {
+    return(
+        <>
+        <Person />
+        </>
+    )
+}
+export default UserComponent;
